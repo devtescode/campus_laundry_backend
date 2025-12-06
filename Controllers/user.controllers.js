cf49eb0f75060ce4624afa117c5893798539ebae
devtescode <teslimagboola09@gmail.com>
2025-12-06 17:37:12 +0100

    update backend code

--- Controllers/user.controllers.js
+++ Controllers/user.controllers.js
@@ -29,6 +29,7 @@ const transporter = nodemailer.createTransport({
 
 
 module.exports.signup = async (req, res) => {
+    console.log(req.body)
     try {
         const { fullname, email, phonenumber, password } = req.body;
 
@@ -114,9 +115,11 @@ module.exports.login = async (req, res) => {
                 phonenumber: user.phonenumber,
                 token
             }
+            
         });
+        console.log(user, "user");
 
-    } catch (err) {
+    } catch (err) { 
         console.log(err);
         res.status(500).json({ message: "Server error" });
     }
